f4ac82821260d1aace53fdfabea1799680009a00
Sathees Kumar K <65384711+sathees-saty@users.noreply.github.com>
2024-04-03 09:38:05 +0530

    Add files via upload

---
+++ react-form-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExpenseForm from './components/ExpenseForm'
+
+ReactDOM.render(
+   <React.StrictMode>
+      <ExpenseForm />
+   </React.StrictMode>,
+   document.getElementById('root')
+);
